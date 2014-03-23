1a2fe73c216c36d8bdf4a59a5677abba916822ac
Ben Krawitz <benkraw@umich.edu>
2014-03-23 16:21:31 -0400

    email works nubs

---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+	$msg = "Thank you for your info";			//EMail message
+	
+	foreach($_POST as $name => $value) {
+		print "$name : $value<br>";
+		$msg .="$name : $value\n";
+	}
+	$to = "bskrawitz@gmail.com";//Put in your email here
+	$headers = "From: ". $_POST["fName"] ."<".$_POST["email1"]. ">\r\n";	
+	mail($to, 'Registration', $msg,$headers);
+	echo "Email sent";
+?>
