6110464829112fb0311d90565aea3c955f4e477a
Ben Krawitz <benkraw@umich.edu>
2014-03-23 16:32:05 -0400

    email

--- process.php
+++ process.php
@@ -2,11 +2,11 @@
 	$msg = "Thank you for your info";			//EMail message
 	
 	foreach($_POST as $name => $value) {
-		print "$name : $value<br>";
 		$msg .="$name : $value\n";
 	}
 	$to = $_POST['email1'];//Put in your email here
 	$headers = "From: ". $_POST["fName"] ."<".$_POST["email1"]. ">\r\n";	
 	mail($to, 'Registration', $msg,$headers);
-	echo "Email sent";
+	echo "Email sent <br>";
+	echo "We Love March Madness";
 ?>
