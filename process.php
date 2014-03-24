5756886b373d23aae0cc3b5cd175c5310d1fc7fb
Ben Krawitz <benkraw@umich.edu>
2014-03-23 22:02:14 -0400

    changed some stuff

--- process.php
+++ process.php
@@ -1,19 +1,19 @@
 <?php
 	echo "<link rel='stylesheet' type='text/css' href='css/heart.css' >";
 
-	$msg = "Thank you for your info";			//EMail message
+	$msg = "Thank you for your info";		
 	
 	foreach($_POST as $name => $value) {
 		$msg .="$name : $value\n";
 	}
-	$to = $_POST['email1'];//Put in your email here
+	$to = $_POST['email1'];
 	$headers = "From: ". $_POST["fName"] ."<".$_POST["email1"]. ">\r\n";	
 	mail($to, 'Registration', $msg,$headers);
 	echo "<p class ='sent'>We Love March Madness, thank you for the email!</p><br>";
 	echo "<div id='background'></div>
-<div id='chest'>
-	<div class='heart left side top'></div>
-	<div class='heart center'>&hearts;</div>
-	<div class='heart right side'></div>
-</div>";
+			<div id='chest'>
+			<div class='heart left side top'></div>
+			<div class='heart center'>&hearts;</div>
+			<div class='heart right side'></div>
+			</div>";
 ?>
