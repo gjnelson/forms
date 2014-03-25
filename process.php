024a2f9002e0df2c899cc6273a29f87769cbc273
Ben Krawitz <benkraw@umich.edu>
2014-03-25 13:45:54 -0400

    updates to process and basically done

--- process.php
+++ process.php
@@ -1,14 +1,16 @@
 <?php
 	echo "<link rel='stylesheet' type='text/css' href='css/heart.css' >";
 
-	$msg = "Thank you for your info";		
-	
-	foreach($_POST as $name => $value) {
-		$msg .="$name : $value\n";
-	}
+	$msg = "Thank you for your info!" . 
+	"\nYour South Pick is: " . $_POST['south'] . 
+	"\nYour West Pick is: " . $_POST['west'] . 
+	"\nYour East Pick is: " . $_POST['east'] . 
+	"\nYour Midwest Pick is: " . $_POST['midwest'];
+
+
 	$to = $_POST['email1'];
 	$headers = "From: ". $_POST["fName"] ."<".$_POST["email1"]. ">\r\n";	
-	mail($to, 'Registration', $msg,$headers);
+	mail($to, 'March Madness Bracket Information!', $msg, $headers);
 	echo "<p class ='sent'>We Love March Madness, thank you for the email!</p><br>";
 	echo "<div id='background'></div>
 			<div id='chest'>
